ac8efca71dab053dd6b0823f319828fc92effae4
Dylan Gardner <dylan@gardnermedia.com>
2020-06-01 21:59:26 -0600

    Create -d/--debug flag for userctld

--- src/userctld.c
+++ src/userctld.c
@@ -2,6 +2,7 @@
 #define _GNU_SOURCE
 #include <assert.h>
 #include <errno.h>
+#include <getopt.h>
 #include <pthread.h>
 #include <signal.h>
 #include <stddef.h>
@@ -12,7 +13,6 @@
 
 #include "controller.h"
 
-void show_help();
 static void* class_enforcer(void* vargp);
 
 static const sd_bus_vtable userctld_vtable[] = {
@@ -31,11 +31,69 @@ static const sd_bus_vtable userctld_vtable[] = {
 static const char* service_path = "/org/dylangardner/userctl";
 static const char* service_name = "org.dylangardner.userctl";
 
+void parse_args(int argc, char* argv[])
+{
+    static int version, help, stop, debug;
+
+    while (true) {
+        static struct option long_options[] = {
+            { "debug", no_argument, &debug, 'd' },
+            { "help", no_argument, &help, 'h' },
+            { "version", no_argument, &version, 'v' },
+            { 0 }
+        };
+
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "dhv", long_options, &option_index);
+        if (c == -1)
+            break;
+        switch (c) {
+        case 'd':
+            debug = 1;
+            break;
+        case 'v':
+            version = 1;
+            break;
+        case 'h':
+            help = 1;
+            break;
+        case '?':
+            stop = 1;
+            break;
+        default:
+            continue;
+        }
+    }
+
+    // Abort, missing/wrong args (getopt will print errors out)
+    if (stop)
+        exit(1);
+
+    if (help) {
+        printf("userctld [OPTIONS...]\n\n"
+               "Sets configurable and persistent resource controls on users and "
+               "groups.\n\n"
+               "  -d --debug\t\tDebugging verbosity is turned on and sent to stderr.\n"
+               "  -h --help\t\tShow this help.\n"
+               "  -v --version\t\tPrint version and exit.\n\n");
+        exit(0);
+    }
+    if (version) {
+        printf("userctld v0.0.1\n");
+        exit(0);
+    }
+    if (debug) {
+        setlogmask(LOG_UPTO(LOG_DEBUG));
+        openlog(NULL, LOG_PERROR | LOG_CONS | LOG_PID | LOG_NDELAY, LOG_DAEMON);
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    // TODO: Implement a -v flag and don't always log to stderr
     setlogmask(LOG_UPTO(LOG_NOTICE));
-    openlog(NULL, LOG_PERROR | LOG_CONS | LOG_PID | LOG_NDELAY, LOG_DAEMON);
+    openlog(NULL, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_DAEMON);
+
+    parse_args(argc, argv);
 
     Context* context = malloc(sizeof *context);
     if (!context || init_context(context) < 0)
@@ -142,7 +200,7 @@ class_enforcer(void* vargp)
         goto cleanup;
     }
 
-    syslog(LOG_INFO, "Preparing to run event loop...");
+    syslog(LOG_INFO, "Running class enforcer event loop...");
     r = sd_event_loop(event);
     if (r < 0) {
         syslog(LOG_ERR, "Failed to run event loop: %s", strerror(-r));
@@ -152,12 +210,4 @@ class_enforcer(void* vargp)
 cleanup:
     sd_bus_flush_close_unref(bus);
     return NULL;
-}
-
-void show_help()
-{
-    printf("userctld [OPTIONS...]\n\n"
-           "Sets configurable and persistent resource controls on users and "
-           "groups.\n\n"
-           "  -h --help\t\tShow this help.\n\n");
 }
